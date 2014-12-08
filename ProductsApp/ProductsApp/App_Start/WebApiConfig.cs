a41c5bfaec10c4fccb58dc0c977ff8730715b535
rocky0904 <rocky0904@gmail.com>
2014-12-07 20:14:55 -0800

    Adding Products App
    This project demonstrates concepts below.
    repository pattern
    dependency injection
    web api

---
+++ ProductsApp/ProductsApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Unity;
+using ProductsApp.App_Start;
+using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ProductsApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var container = new UnityContainer();            
+            container.RegisterType<IProductRepository, ProductRepository>(new HierarchicalLifetimeManager());
+            config.DependencyResolver = new UnityResolver(container);
+        }
+    }
+}
---
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IProductRepository _repository ;
+
+        public ProductsController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _repository.GetAllProducts();
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = _repository.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
